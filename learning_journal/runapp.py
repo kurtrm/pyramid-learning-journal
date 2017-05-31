80101b577fa38e18fa68b208c8739a9c922816d3
Kurt <kurtrm@gmail.com>
2017-05-31 13:27:56 -0700

    Tried some more changes.

--- learning_journal/runapp.py
+++ learning_journal/runapp.py
@@ -1,10 +1,9 @@
-"""This is a docstring for the runapp module."""
 import os
 
 from paste.deploy import loadapp
 from waitress import serve
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:production.ini', relative_to='.')
 
