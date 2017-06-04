30ed51fc336d6103e26586a9e798b1ba0b51c56a
kurtrm <kurtrm@gmail.com>
2017-06-04 10:46:22 -0700

    Added the forms, testing non-functionality.

--- learning_journal/learning_journal/views/default.py
+++ learning_journal/learning_journal/views/default.py
@@ -1,7 +1,7 @@
 from pyramid.view import view_config
-from learning_journal.data.entries import ENTRIES
-from pyramid.httpexceptions import HTTPNotFound
+from pyramid.httpexceptions import HTTPNotFound, HTTPFound
 from learning_journal.models.entries import Entry
+import datetime
 
 
 @view_config(
@@ -27,7 +27,7 @@ def detail_view(request):
     entry = request.dbsession.query(Entry).get(entry_id)
     if entry:
         return {
-            'id': entry.id,
+            'entry_id': entry.id,
             'title': entry.title,
             'body': entry.body,
             'creation_date': entry.creation_date
@@ -35,19 +35,50 @@ def detail_view(request):
     else:
         raise HTTPNotFound
 
+
 @view_config(
     route_name="create_view",
     renderer='../templates/new_entry.jinja2'
 )
 def create_view(request):
-    """Create new view."""
-    return {}
+    """Create new entry."""
+    if request.method == "POST" and request.POST:
+        if not request.POST['title'] or not request.POST['body']:
+            return {
+                'title': request.POST['title'],
+                'body': request.POST['body'],
+                'error': 'Please provide a title and/or body.'
+            }
+        new_entry = Entry(
+            title=request.POST['title'],
+            body=request.POST['body'],
+            creation_date=datetime.datetime.now()
+        )
+        request.dbsession.add(new_entry)
+        return HTTPFound(
+            location=request.route_url('list_view')
+        )
 
 
-# @view_config(
-#     route_name="update_view",
-#     renderer='../templates/edit.jinja2'
-# )
-# def update_view(request):
-#     """Update existing view."""
-#     return {}
+@view_config(
+    route_name="update_view",
+    renderer='../templates/edit.jinja2'
+)
+def update_view(request):
+    """Update existing journal entry."""
+    the_id = int(request.matchdict['id'])
+    session = request.dbsession
+    entry = session.query(Entry).get(the_id)
+    if not entry:
+        raise HTTPNotFound
+    if request.method == 'GET':
+        return {
+            'page': 'Edit Entry',
+            'title': entry.title,
+            'body': entry.body,
+        }
+    if request.method == "POST":
+        entry.title = request.POST['title']
+        entry.body = request.POST['body']
+        request.dbsession.flush()
+        return HTTPFound(request.route_url('detail', id=entry.id))
